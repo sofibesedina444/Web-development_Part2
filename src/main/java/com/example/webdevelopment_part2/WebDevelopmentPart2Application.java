b8cafb9e2e3fd34ebc778c46b1e4d7b3a337559a
salva <salvationordeath@gmail.com>
2022-12-27 20:48:51 +0700

    Первый коммит

---
+++ src/main/java/com/example/webdevelopment_part2/WebDevelopmentPart2Application.java
@@ -0,0 +1,13 @@
+package com.example.webdevelopment_part2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDevelopmentPart2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebDevelopmentPart2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webdevelopment_part2/WebDevelopmentPart2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webdevelopment_part2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDevelopmentPart2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
